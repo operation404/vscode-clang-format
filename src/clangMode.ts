c24d71cae3a9cf49b36ef540d0f1bed1aec8e0dc
operation404 <operation404@gmail.com>
2024-05-23 15:41:31 -0700

    Quick modification to add support for godot shader files (.gdshader) which are based off glsl.

--- src/clangMode.ts
+++ src/clangMode.ts
@@ -3,15 +3,34 @@
 import * as vscode from 'vscode';
 
 export const ALIAS = {
-  'proto3': 'proto'
+    proto3: 'proto',
 };
 
 let languages: string[] = [];
-for (let l of ['cpp', 'c', 'csharp', 'objective-c', 'objective-cpp', 'java', 'javascript', 'json', 'typescript', 'proto', 'proto3', 'textproto', 'apex', 'glsl', 'hlsl', 'cuda', 'cuda-cpp']) {
-  let confKey = `language.${ALIAS[l] || l}.enable`;
-  if (vscode.workspace.getConfiguration('clang-format').get(confKey)) {
-    languages.push(l);
-  }
+for (let l of [
+    'cpp',
+    'c',
+    'csharp',
+    'objective-c',
+    'objective-cpp',
+    'java',
+    'javascript',
+    'json',
+    'typescript',
+    'proto',
+    'proto3',
+    'textproto',
+    'apex',
+    'glsl',
+    'hlsl',
+    'cuda',
+    'cuda-cpp',
+    'godot-shader',
+]) {
+    let confKey = `language.${ALIAS[l] || l}.enable`;
+    if (vscode.workspace.getConfiguration('clang-format').get(confKey)) {
+        languages.push(l);
+    }
 }
 
-export const MODES: vscode.DocumentFilter[] = languages.map((language) => ({language, scheme: 'file'}));
+export const MODES: vscode.DocumentFilter[] = languages.map((language) => ({ language, scheme: 'file' }));
